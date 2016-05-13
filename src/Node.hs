811bfa65662603e30de4bfda795c2ec8d242d865
stites <sam@stites.io>
2016-05-12 20:07:09 -0700

    create a node

--- src/Node.hs
+++ src/Node.hs
@@ -1,3 +1,25 @@
-module Node where
+module Node (
+    Node(..)
+  , runNode
+  ) where
 
-output = "arst"
+import Data.Functor ()
+import Control.Monad ()
+import Control.Applicative ()
+
+data Node a = Node a
+
+runNode :: Node a -> a
+runNode (Node a) = a
+
+instance Functor Node where
+  fmap f (Node a) = Node (f a)
+
+instance Applicative Node where
+  pure a = Node a
+  (Node fab) <*> (Node b) = Node (fab b)
+
+instance Monad Node where
+  return = pure
+  (Node a) >>= faNb = faNb a
+
--- test/NodeSpec.hs
+++ test/NodeSpec.hs
@@ -1,12 +1,13 @@
 module NodeSpec where
 
+import Node
+
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
-import Node
 
 spec =
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+  describe "Getting a value from a single node" $ do
+    it "should return the value of the node" $ do
+      runNode (Node 1) `shouldBe` (1 :: Int)
 
